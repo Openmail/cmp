c1c923ef9b7c64d4ef2b1c401ba35cadad542871
Christian Shum-Harden <christian.harden@gmail.com>
2018-10-31 13:18:17 -0700

    update default template strings to new english translations for fallback across regions

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -52,7 +52,7 @@ export default class Footer extends Component {
 				/>
 				<div>
 					<LocalLabel localizeKey="message" class={style.title}>
-						<h2>A reminder you can control your user privacy preferences</h2>
+						<h2>We Value Privacy</h2>
 					</LocalLabel>
 					<div class={style.descriptionRow}>
 						<LocalLabel localizeKey="description" class={style.message}>
@@ -60,26 +60,31 @@ export default class Footer extends Component {
 								To help make this website better, to personalize and enhance
 								your content experience, for advertising purposes and to analyze
 								our traffic, we and our partners use technology such as cookies,
-								[pixels, tokens and/or beacons] to collect certain data. By
+								pixels, and/or beacons to collect certain data. By
 								continuing to use the site or clicking “OK”, you agree to the
 								use of this technology and collecting the data.
 							</span>
 						</LocalLabel>
 						<LocalLabel localizeKey="privacyPolicy" class={style.message}>
 							<span>
-								Please visit our Privacy Policy [Link to PP] to learn more about
-								how we collect and use data. You can modify your settings at any
-								time by clicking [Manage Settings Link].
+								Please visit our
+								<a target="_blank" href="http://system1.com/terms/privacy.html">
+									Privacy Policy
+								</a>
+								to learn more about how we collect and use data. You can modify
+								your settings at any time by clicking
 							</span>
 						</LocalLabel>
 						<a class={style.modalLink} onClick={this.handleShowConsent}>
-							<LocalLabel localizeKey="privacyPolicyButton">Manage Privacy Settings</LocalLabel>
+							<LocalLabel localizeKey="privacyPolicyButton">
+								Manage Privacy Settings
+							</LocalLabel>
 						</a>
 					</div>
 				</div>
 				<div class={style.consentRow}>
 					<a class={style.consentBtn} onClick={this.onAcceptAll}>
-						<LocalLabel localizeKey="consentLink">here</LocalLabel>
+						<LocalLabel localizeKey="consentLink">OK</LocalLabel>
 					</a>
 				</div>
 			</div>
